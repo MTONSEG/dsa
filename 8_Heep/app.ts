29e36103a03b3c47a53090667e3453dd6fb637b5
Maksym Puzanov <puzanov.maksym@gmail.com>
2023-03-31 13:22:46 +0300

    update heep

--- 8_Heep/app.ts
+++ 8_Heep/app.ts
@@ -1,158 +1,85 @@
-// class Heep {
-// 	private heep: number[] = [0];
-// 	private sorted: number[] = [];
-
-// 	insert(value: number) {
-// 		this.heep.push(value);
-
-// 		let index: number = this.heep.length - 1;
-
-// 		while (index > 1 && this.heep[index] < this.heep[Math.floor(index / 2)]) {
-// 			let tmp: number = this.heep[index];
-
-// 			this.heep[index] = this.heep[Math.floor(index / 2)];
-// 			this.heep[Math.floor(index / 2)] = tmp;
-
-// 			index = Math.floor(index / 2);
-// 		}
-// 	}
-
-// 	delete(index: number): number {
-// 		if (this.heep.length > 2) {
-// 			let value = this.heep[index];
-// 			this.heep[index] = this.heep.pop();
-
-// 			let lastIndex: number = this.heep.length - 1;
-
-// 			while (index * 2 <= lastIndex) {
-// 				let minEl: number = index * 2;
-
-// 				if (minEl + 1 <= lastIndex && this.heep[minEl] > this.heep[minEl + 1]) {
-// 					minEl += 1;
-// 				}
-
-// 				if (this.heep[index] > this.heep[minEl]) {
-// 					let tmp: number = this.heep[index];
-// 					this.heep[index] = this.heep[minEl];
-// 					this.heep[minEl] = tmp;
-// 				}
-
-// 				index = minEl;
-// 			}
-
-// 			return value;
-// 		} else if (this.heep.length == 2) {
-// 			return this.heep.pop();
-// 		}
-// 	}
-
-// 	get data(): number[] {
-// 		return this.heep
-// 	}
-
-// 	sort() {
-// 		while (this.heep.length > 1) {
-// 			this.sorted.push(this.delete(1));
-// 		}
-// 	}
-// }
-
-// let heep = new Heep();
-
-// let arr: number[] = [1, 3, 3, 9, 3, 2, 4, 5, 34, 24];
-
-// for (let item of arr) heep.insert(item);
-
-// console.log(heep.data);
-// console.log(heep);
-// heep.sort();
-// console.log(heep);
-
-
-
-class MaxHeep {
-	private heep: number[] = [];
+class HeepMin {
+	private storage: number[] = [];
 	private sorted: number[] = [];
 
-	insert(num: number) {
-		this.heep.push(num);
+	constructor(num: number) {
+		this.fill(num);
+	}
 
-		if (this.heep.length > 1) {
-			let index: number = this.heep.length - 1;
-			let parent: number = this.parentIndex(index);
+	insert(value: number): void {
+		this.storage.push(value);
 
-			while (this.heep[parent] && this.heep[index] > this.heep[parent]) {
-				this.swap(index, parent);
-				index = this.parentIndex(index);
-				parent = this.parentIndex(index);
-			}
+		let currentIndex: number = this.storage.length - 1;
+		let parentIndex: number = this.parentIndex(currentIndex);
+
+		while (this.storage[currentIndex] && this.storage[parentIndex] > this.storage[currentIndex]) {
+			this.swap(this.storage, currentIndex, parentIndex);
+			currentIndex = parentIndex;
+			parentIndex = this.parentIndex(currentIndex);
 		}
 	}
 
-	delete(): number {
-		if (this.heep.length > 1) {
-			let value: number = this.heep.shift();
-			this.heep.unshift(this.heep.pop());
-
-			console.log(this.heep);
-
+	remove(): number {
+		if (this.storage.length > 1) {
 			let index: number = 0;
-			let leftChild: number = this.leftChildIndex(index);
-			let rightChild: number = this.rightChildIndex(index);
+			let leftChildIndex: number = this.leftChildIndex(index);
+			let rightChildIndex: number = this.rightChildIndex(index);
+			let maxChildIndex = leftChildIndex;
 
+			let value: number = this.storage[index];
+			this.storage[index] = this.storage.pop();
 
-			while (this.heep[leftChild] && index <= this.heep.length - 1) {
-				let maxChild = leftChild;
+			while (this.storage[leftChildIndex] && this.storage.length - 1 > index) {
+				if (this.storage[rightChildIndex] && this.storage[rightChildIndex] < this.storage[maxChildIndex]) {
+					maxChildIndex = rightChildIndex
+				};
 
-				if (this.heep[rightChild] && this.heep[rightChild] > this.heep[leftChild]) {
-					maxChild = rightChild;
-				}
+				if (this.storage[index] > this.storage[maxChildIndex]) {
+					this.swap(this.storage, maxChildIndex, index)
+				};
 
-				this.swap(index, maxChild);
-
-				index = maxChild;
-				leftChild = this.leftChildIndex(index);
-				rightChild = this.rightChildIndex(index);
-				console.log(this.heep);
+				index = maxChildIndex;
+				leftChildIndex = this.leftChildIndex(index);
+				maxChildIndex = leftChildIndex;
 			}
 
 			return value;
 		} else {
-			return this.heep.pop();
+			return this.storage.pop();
 		}
 	}
 
-	// sort(): void {
-	// 	let size: number = this.heep.length;
-
-	// 	for (let i: number = 0; i < size; i++) {
-	// 		this.sorted.push(this.delete());
-	// 	}
-	// }
-
-	getHeep = (): number[] => this.heep;
-
-	parentIndex = (index: number): number => Math.floor((index - 1) / 2);
-	leftChildIndex = (index: number): number => Math.floor((index * 2) + 1);
-	rightChildIndex = (index: number): number => Math.floor((index * 2) + 2);
-
-	swap(a: number, b: number) {
-		let tmp: number = this.heep[a];
-		this.heep[a] = this.heep[b];
-		this.heep[b] = tmp;
+	sort(): void {
+		while (this.storage.length > 0) {
+			this.sorted.push(this.remove());
+		}
 	}
 
-	fill(arr: number[]): void {
-		for (let elem of arr) this.insert(elem);
+	fill(num: number) {
+		for (let i: number = 0; i < num; i++) {
+			let random: number = Math.floor(Math.random() * ((num * 2) - 1) + 1);
+
+			this.insert(random);
+		}
+	}
+
+	getStorage = (): number[] => this.storage;
+	addSort = (num: number): void => { this.sorted.push(num) };
+
+	private parentIndex = (index: number): number => Math.floor((index - 1) / 2);
+	private leftChildIndex = (index: number): number => Math.floor((index * 2) + 1);
+	private rightChildIndex = (index: number): number => Math.floor((index * 2) + 2);
+
+	private swap<T>(arr: T[], index1: number, index2: number) {
+		let tmp: T = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = tmp;
 	}
 }
 
-let heep = new MaxHeep();
+let heepMin = new HeepMin(7);
 
-let arr: number[] = [34, 22, 1, 2, 3434, 23, 43, 58];
+heepMin.sort();
 
-heep.fill(arr);
+console.log('End');
 
-console.log(heep);
-
-console.log(heep);
--- 8_Heep/dist/app.js
+++ 8_Heep/dist/app.js
@@ -1,120 +1,74 @@
-// class Heep {
-// 	private heep: number[] = [0];
-// 	private sorted: number[] = [];
-// 	insert(value: number) {
-// 		this.heep.push(value);
-// 		let index: number = this.heep.length - 1;
-// 		while (index > 1 && this.heep[index] < this.heep[Math.floor(index / 2)]) {
-// 			let tmp: number = this.heep[index];
-// 			this.heep[index] = this.heep[Math.floor(index / 2)];
-// 			this.heep[Math.floor(index / 2)] = tmp;
-// 			index = Math.floor(index / 2);
-// 		}
-// 	}
-// 	delete(index: number): number {
-// 		if (this.heep.length > 2) {
-// 			let value = this.heep[index];
-// 			this.heep[index] = this.heep.pop();
-// 			let lastIndex: number = this.heep.length - 1;
-// 			while (index * 2 <= lastIndex) {
-// 				let minEl: number = index * 2;
-// 				if (minEl + 1 <= lastIndex && this.heep[minEl] > this.heep[minEl + 1]) {
-// 					minEl += 1;
-// 				}
-// 				if (this.heep[index] > this.heep[minEl]) {
-// 					let tmp: number = this.heep[index];
-// 					this.heep[index] = this.heep[minEl];
-// 					this.heep[minEl] = tmp;
-// 				}
-// 				index = minEl;
-// 			}
-// 			return value;
-// 		} else if (this.heep.length == 2) {
-// 			return this.heep.pop();
-// 		}
-// 	}
-// 	get data(): number[] {
-// 		return this.heep
-// 	}
-// 	sort() {
-// 		while (this.heep.length > 1) {
-// 			this.sorted.push(this.delete(1));
-// 		}
-// 	}
-// }
-// let heep = new Heep();
-// let arr: number[] = [1, 3, 3, 9, 3, 2, 4, 5, 34, 24];
-// for (let item of arr) heep.insert(item);
-// console.log(heep.data);
-// console.log(heep);
-// heep.sort();
-// console.log(heep);
-class MaxHeep {
-    constructor() {
-        this.heep = [];
+class HeepMin {
+    constructor(num) {
+        this.storage = [];
         this.sorted = [];
-        this.getHeep = () => this.heep;
+        this.getStorage = () => this.storage;
+        this.addSort = (num) => { this.sorted.push(num); };
         this.parentIndex = (index) => Math.floor((index - 1) / 2);
         this.leftChildIndex = (index) => Math.floor((index * 2) + 1);
         this.rightChildIndex = (index) => Math.floor((index * 2) + 2);
+        this.fill(num);
     }
-    insert(num) {
-        this.heep.push(num);
-        if (this.heep.length > 1) {
-            let index = this.heep.length - 1;
-            let parent = this.parentIndex(index);
-            while (this.heep[parent] && this.heep[index] > this.heep[parent]) {
-                this.swap(index, parent);
-                index = this.parentIndex(index);
-                parent = this.parentIndex(index);
-            }
+    insert(value) {
+        this.storage.push(value);
+        let currentIndex = this.storage.length - 1;
+        let parentIndex = this.parentIndex(currentIndex);
+        while (this.storage[currentIndex] && this.storage[parentIndex] > this.storage[currentIndex]) {
+            this.swap(this.storage, currentIndex, parentIndex);
+            currentIndex = parentIndex;
+            parentIndex = this.parentIndex(currentIndex);
         }
     }
-    delete() {
-        if (this.heep.length > 1) {
-            let value = this.heep.shift();
-            this.heep.unshift(this.heep.pop());
-            console.log(this.heep);
+    remove() {
+        if (this.storage.length > 1) {
             let index = 0;
-            let leftChild = this.leftChildIndex(index);
-            let rightChild = this.rightChildIndex(index);
-            while (this.heep[leftChild] && index <= this.heep.length - 1) {
-                let maxChild = leftChild;
-                if (this.heep[rightChild] && this.heep[rightChild] > this.heep[leftChild]) {
-                    maxChild = rightChild;
+            let leftChildIndex = this.leftChildIndex(index);
+            let rightChildIndex = this.rightChildIndex(index);
+            let maxChildIndex = leftChildIndex;
+            let value = this.storage[index];
+            this.storage[index] = this.storage.pop();
+            while (this.storage[leftChildIndex] && this.storage.length - 1 > index) {
+                if (this.storage[rightChildIndex] && this.storage[rightChildIndex] < this.storage[maxChildIndex]) {
+                    maxChildIndex = rightChildIndex;
                 }
-                this.swap(index, maxChild);
-                index = maxChild;
-                leftChild = this.leftChildIndex(index);
-                rightChild = this.rightChildIndex(index);
-                console.log(this.heep);
+                ;
+                if (this.storage[index] > this.storage[maxChildIndex]) {
+                    this.swap(this.storage, maxChildIndex, index);
+                }
+                ;
+                index = maxChildIndex;
+                leftChildIndex = this.leftChildIndex(index);
+                maxChildIndex = leftChildIndex;
             }
             return value;
         }
         else {
-            return this.heep.pop();
+            return this.storage.pop();
         }
     }
     sort() {
-        let size = this.heep.length;
-        for (let i = 0; i < size; i++) {
-            this.sorted.push(this.delete());
+        while (this.storage.length > 0) {
+            this.sorted.push(this.remove());
         }
     }
-    swap(a, b) {
-        let tmp = this.heep[a];
-        this.heep[a] = this.heep[b];
-        this.heep[b] = tmp;
+    fill(num) {
+        for (let i = 0; i < num; i++) {
+            let random = Math.floor(Math.random() * ((num * 2) - 1) + 1);
+            this.insert(random);
+        }
     }
-    fill(arr) {
-        for (let elem of arr)
-            this.insert(elem);
+    swap(arr, index1, index2) {
+        let tmp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = tmp;
     }
 }
-let heep = new MaxHeep();
-let arr = [34, 22, 1, 2, 3434, 23, 43, 58];
-heep.fill(arr);
-console.log(heep);
-heep.sort();
-console.log(heep);
+let heepMin = new HeepMin(7);
+// heepMin.addSort(heepMin.remove());
+// heepMin.addSort(heepMin.remove());
+// heepMin.addSort(heepMin.remove());
+// heepMin.addSort(heepMin.remove());
+// heepMin.addSort(heepMin.remove());
+heepMin.sort();
+console.log('End');
 //# sourceMappingURL=app.js.map
